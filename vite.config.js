0cf018c02260a62f811203025a80c55156b8e70c
MegaDev007 <byddev804@gmail.com>
2024-12-29 10:23:50 -0500

    updated server address and language skills

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio-template/',
+  base: "/react-portfolio-template/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['mixed-decls'],
+        silenceDeprecations: ["mixed-decls"],
       },
     },
   },
-})
+  server: {
+    host: "0.0.0.0", // Allows connections from any IP (used for private IPs)
+    port: 5173, // Change the port if needed (default is 5173)
+  },
+});
