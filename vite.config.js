850867c0d150dbbcb97b5ae402582889eb307420
MegaDev007 <byddev804@gmail.com>
2025-02-28 13:50:30 -0500

    fix all

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/",
+  base: "/Mega-Portfolio/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
@@ -12,13 +11,8 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
   server: {
-    host: "0.0.0.0", // Allows connections from any IP (used for private IPs)
-    port: 5173, // Change the port if needed (default is 5173)
+    host: "0.0.0.0",
+    port: 5173,
   },
 });
